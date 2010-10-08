580c03f2176a8146a9c3361020f71cffabc1ac89
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-08 17:51:21 +0200

    re-annotate models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,21 +1,11 @@
 # == Schema Information
 #
-# Table name: codes
+# Table name: locations
 #
-#  id                  :integer         not null, primary key
-#  parent_id           :integer
-#  lft                 :integer
-#  rgt                 :integer
-#  short_display       :string(255)
-#  long_display        :string(255)
-#  description         :text
-#  created_at          :datetime
-#  updated_at          :datetime
-#  start_date          :date
-#  end_date            :date
-#  replacement_code_id :integer
-#  type                :string(255)
-#  external_id         :string(255)
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
 #
 
 class Location < ActiveRecord::Base
